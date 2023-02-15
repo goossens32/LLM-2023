cdfd6bc7dee51e318847c69dab4c36fd74430415
goossens32 <cgp00@iesemilidarder.com>
2023-02-15 17:38:54 +0100

    ACT-4.2 / EJ2 Terminado

--- UT4/ACT-4.2/EJ2 - StarWars/js/app.js
+++ UT4/ACT-4.2/EJ2 - StarWars/js/app.js
@@ -6,25 +6,76 @@ const btnFiltrarTag = document.getElementById("btn-filter");
 
 let figuresList = [];
 
-// TODO:Petició asíncona per recuperar les figures
+/*
+La función espera a recoger los datos del JSON antes de seguir con el código
+async function getFigures(){
+    const response = await fetch('./data/star-wars-figures.json')
+    const data = await response.json();
+    printFigures(data)
+}
+*/
+
+// Petició asíncona per recuperar les figures
 function getFigures() {
-
+    fetch('./data/star-wars-figures.json')
+        .then(res => res.json())
+        .then(data => {
+            figuresList = data;
+            printFigures(figuresList);
+    });
 }
 
-
-// TODO:Crea les cards HTML de cada figura 
+// Crea les cards HTML de cada figura 
 function printFigures(listJSON) {
-   
+    listTag.innerHTML = "";
+
+    listJSON.forEach(figure => {
+        listTag.innerHTML += `
+        <article class="card">
+            <img src="./img/${figure.photo}" alt="">
+            <h3>${figure.name}</h3>
+            <span>${figure.price} €</span>
+            <div class="favorite">
+                <i class="fas fa-heart"></i>
+            </div>
+        </article>
+        `    
+    });
+    setFavourites();
 }
 
-// TODO: Intercanvia el icone de favorit
+// Intercanvia el icone de favorit
 function setFavourites() {
-
+    // Crea constante All para clase de font awesome
+    const favoriteIcon = document.querySelectorAll('.fa-heart');
+        // Por cada icono añade función on click
+        favoriteIcon.forEach(icon => {
+            icon.addEventListener("click", function(){
+                // Si la clase ".ON" no está activada la activará, si está ya activada la eliminará.
+                // En este caso "on" cambia de color el corazón de FontAwesome cada vez que se haga click en el.
+                this.classList.toggle("on");
+            });
+        });
 }
 
 // TODO: Filtra les figures per preu i les ordena
 function filterFigures() {
-  
+    // list = Array JSON con fetch
+    let list = figuresList
+        // Con .filter las condiciones y devuelve un nuevo Array con JSON.
+        .filter(e => (e.price >= priceMinTag.value && e.price <= priceTag.value))
+        // Ordenar. Si alfabeticamente a > que b . a se pondrá a la izquierda
+        // Hará esta función con todos los objetos del Array filtrado
+        .sort((a, b) => {
+            // Ordena de major a menor pel nom
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
+            return 0;
+        });
+        // Si el resultado del filtro no obtiene ninguna figura aparece mensaje.
+        if (list.length === 0) listTag.innerHTML = "No hay figuras que coincidan con el filtro";
+        // Vuelve a "pintar" la lista de figuras pero esta vez filtrado.
+        else printFigures(list)
 }
 
 /*
