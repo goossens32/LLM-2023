11d93b9bd0b39e88caae1c419c7ff86422b10f16
goossens32 <cgp00@iesemilidarder.com>
2023-02-15 16:12:22 +0100

    Nombres cambiados y ACT4.2 empezada

---
+++ UT4/ACT-4.2/EJ2 - StarWars/js/app.js
@@ -0,0 +1,53 @@
+// Contants amb les referencies als elements HTML
+const listTag = document.querySelector('#figures-list');
+const priceTag = document.querySelector('#filter-price');
+const priceMinTag = document.querySelector('#filter-min-price');
+const btnFiltrarTag = document.getElementById("btn-filter");
+
+let figuresList = [];
+
+// TODO:Petició asíncona per recuperar les figures
+function getFigures() {
+
+}
+
+
+// TODO:Crea les cards HTML de cada figura 
+function printFigures(listJSON) {
+   
+}
+
+// TODO: Intercanvia el icone de favorit
+function setFavourites() {
+
+}
+
+// TODO: Filtra les figures per preu i les ordena
+function filterFigures() {
+  
+}
+
+/*
+// Filtre amb programació funcional
+function filterFigures() {
+    let list =[];
+    for (let figure of figuresList){
+        // El valor de la figura debe estar entre el mínimo y el máximo especificado
+        if (figure.price>=priceMinTag.value && figure.price<=priceTag.value ){
+            list.push(figure);
+        }
+    }
+    if (list.length == 0) {
+        listTag.innerHTML = "No hay figuras que coincidan con el filtro";
+    } else {
+        printFigures(list);
+    }
+}
+*/
+
+// Funció inicial de càrrega de la página
+function init() {
+    getFigures();
+    btnFiltrarTag.addEventListener("click", filterFigures);
+}
+init();
